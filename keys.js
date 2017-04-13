c9c8543b249ecaaea2f4cc120f01abe122ff348e
Mohammed Reshid <Mohammed Reshid>
2017-04-13 18:03:52 -0400

    final commit

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
-﻿console.log('this is loaded);
+﻿console.log('this is loaded');
 
 exports.twitterKeys = {
-	consumer_key: '<input here>',
-	consumer_secret: '<input here>',
-	access_token_key: '<input here>',
-	access_token_secret: '<input here>',
+	consumer_key: '<44pE3TWBZAcXCKi6S7Y7HPTF8>',
+	consumer_secret: '<zhHbefqMh2udR9ZhsRMRizGbSAWKgzTpLDvw6hS4f2zTPR1ofh>',
+	access_token_key: '<228681071-A82518SKjpJotkegaVPMFiqfqxYLdCcfIKCoZDl5>',
+	access_token_secret: '<MQtSdM7OZfeSQIvPRSf0Qj5yKdkJekBWlKhWpvNbv0G22>',
 };
--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
-﻿
+﻿// Grab the data from keys.js. 
+var fs = require('fs');
+// Read the file from the txt file
+fs.readFile("random.txt", "utf8", function(error, data) {
+//log the data
+console.log(data);
+});
+
+
+
+
+
